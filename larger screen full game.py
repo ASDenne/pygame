d8238b372da0a2aeb78f90e41cacfdf6b4f27a29
ASDenne <98923598+ASDenne@users.noreply.github.com>
2024-02-26 09:16:54 +1300

    sent in copy

--- larger screen full game.py
+++ larger screen full game.py
@@ -2,7 +2,14 @@ import time
 import random
 import pygame
 pygame.init()
-
+global time
+global space
+global space_time
+global time_time
+time = False
+space = False
+space_time = 0
+time_time = 0
 screen = pygame.display.set_mode((1000,740))
 game_icon = pygame.image.load('snake_icon.png')
 pygame.display.set_icon(game_icon)
@@ -14,7 +21,12 @@ red = (255,0,0)
 green = (148, 227, 159)
 yellow = (255,255,0)
 blue = (0,0,255)
-
+cyan = (0,255,255)
+fruit_plain = green
+fruit_time = yellow
+fruit_space = cyan
+#feature locked or require certain things for when not testing while be behind a if testing
+testing = True
 score_font = pygame.font.SysFont("arialblack",20)
 exit_font = pygame.font.Font("freesansbold.ttf",30)
 msg_font = pygame.font.SysFont("arialblack",20)
@@ -58,6 +70,11 @@ def draw_snake(snake_list,colour):
     #print(f"snake list:{snake_list}")
     for i in snake_list:
         pygame.draw.rect(screen,colour,[i[0],i[1],10, 10])
+        if space and random.randint(0,3)==2:
+            pygame.draw.rect(screen,fruit_space,[i[0],i[1],10, 10])
+        if time and random.randint(0,5)==1:
+            pygame.draw.rect(screen,fruit_time,[i[0],i[1],10, 10])
+
 
 def message(msg,txt_colour,bkgd_colour,x,y):
     #sending a pause or end of game message
@@ -69,6 +86,10 @@ def message(msg,txt_colour,bkgd_colour,x,y):
 def game_loop():
     #the game
     #stats
+    time = False
+    space = False
+    space_time = 0
+    time_time = 0
     quit_game = False
     game_over = False
     size_x = 1000
@@ -81,17 +102,24 @@ def game_loop():
     speed = 5
     temp_speed = speed
     score = 0
-
+    food_count = 5
     snake_x_change = 0
     snake_y_change = 0
-
-    food_x = round(random.randrange(10,1000 - 10)/10)*10
-    food_y = round(random.randrange(10,720 - 10)/10)*10
+    food_x = []
+    food_y = []
+    food_type = []
+    for i in range(food_count):
+        food_x.append(round(random.randrange(10,1000 - 10)/10)*10)
+        food_y.append(round(random.randrange(10,720 - 10)/10)*10)
+        food_type.append(random.choices(["normal","space","time"],[5,1,1]))
+        print(food_type)
 
     high_score = load_high_score()
-    print(f"high_score test: {high_score}")
-
+    #print(f"high_score test: {high_score}")
+    screensizeupdate = True
     while not quit_game:
+        print(space)
+        print(time)
         #screen = pygame.display.set_mode((1000,720))
         while game_over:
             save_high_score(high_score)
@@ -110,12 +138,14 @@ def game_loop():
                         game_loop()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                #if they click the X
                 #screen = pygame.display.set_mode((1000,720))
                 instructions = "Exit: X to quit, SPACE to resume, R to reset"
                 message(instructions,white,black,size_x/2,size_y/2)
                 pygame.display.update()
                 end = False
                 while not end:
+                    #seeing which command is given
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             quit_game = True
@@ -128,14 +158,16 @@ def game_loop():
                             if event.key == pygame.K_x:
                                 quit_game = True
                                 end = True
-
+            #seeing what keys are being pressed for directions
+            # (take out + to set it to just left right up down and not diagonal
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    temp_speed = speed*3
-                elif event.key == pygame.K_s:
-                    temp_speed = speed/2
-                else:
-                    temp_speed = speed
+                if testing or time:
+                    if event.key == pygame.K_w:
+                        temp_speed = speed*5
+                    elif event.key == pygame.K_s:
+                        temp_speed = speed/4
+                    else:
+                        temp_speed = speed
                 if event.key == pygame.K_LEFT and not (snake_y_change == 0 and snake_x_change == 10):
                     snake_y_change += 0
                     snake_x_change += -10
@@ -148,28 +180,33 @@ def game_loop():
                 if event.key == pygame.K_DOWN and not (snake_y_change == -10 and snake_x_change == 0):
                     snake_y_change += 10
                     snake_x_change += 0
-                if snake_x_change > 10:
-                    snake_x_change = 10
-                if snake_y_change > 10:
-                    snake_y_change = 10
-                if snake_x_change < -10:
-                    snake_x_change = -10
-                if snake_y_change < -10:
-                    snake_y_change = -10
+                #limiting increases of movement to 1x and 1y square per turn
+                if not space:
+                    if snake_x_change > 10:
+                        snake_x_change = 10
+                    if snake_y_change > 10:
+                        snake_y_change = 10
+                    if snake_x_change < -10:
+                        snake_x_change = -10
+                    if snake_y_change < -10:
+                        snake_y_change = -10
+        #setting up screen wrapping so that snake reaches other side
         if snake_x >= size_x:
-            snake_x = 5
+            snake_x = -5
         if snake_y >= size_y:
-            snake_y = 5
-        if snake_x < 0:
-            snake_x = size_x-5
-        if snake_y < 0:
-            snake_y = size_y-5
+            snake_y = -5
+        if snake_x < -5:
+            snake_x = size_x+5
+        if snake_y < -5:
+            snake_y = size_y+5
 
         snake_x += snake_x_change
         snake_y += snake_y_change
-
+        if screensizeupdate:
+                    screen = pygame.display.set_mode((size_x,size_y))
+                    screensizeupdate = False
         screen.fill(green)
-
+        #printing the snake
         snake_head = [snake_x,snake_y]
         snake_list.append(snake_head)
         if len(snake_list) > snake_length:
@@ -180,29 +217,57 @@ def game_loop():
         draw_snake(snake_list,red)
         draw_snake([snake_head],blue)
         #pygame.display.update()
+        # setting up and printing scores
         score = snake_length -1
         high_score = update_high_score(score,high_score)
         player_score(score,black,high_score)
-
+        #printing food
+        for i in range(food_count):
         #pygame.draw.circle(screen,yellow,[food_x,food_y],5)
-        food =pygame.Rect(food_x-5,food_y-5,10,10)
-        apple = pygame.image.load('apple_3.png').convert_alpha()
-        resized_apple = pygame.transform.smoothscale(apple,[10,10])
-        screen.blit(resized_apple,food)
+            if food_type[i-1] == ["space"]:
+                pygame.draw.circle(screen,fruit_space,[food_x[i-1],food_y[i-1]],7)
+            elif food_type[i-1] == ["time"]:
+                pygame.draw.circle(screen,fruit_time,[food_x[i-1],food_y[i-1]],7)
+            food = pygame.Rect(food_x[i-1]-5,food_y[i-1]-5,10,10)
+            apple = pygame.image.load('apple_3.png').convert_alpha()
+            resized_apple = pygame.transform.smoothscale(apple,[10,10])
+            screen.blit(resized_apple,food)
+
+
+            #pygame.display.update()
+            #spawning food
+
+            if snake_x == food_x[i-1]-5 and snake_y == food_y[i-1]-5 :
+                food_x[i-1] = round(random.randrange(10,size_x - 10)/10)*10
+                food_y[i-1] = round(random.randrange(10,size_y - 10)/10)*10
+                snake_length += 1
+                size_y = size_y-10
+                size_x = size_x-10
+                screensizeupdate = True
+                speed += 0.1
+                if food_type[i-1] == ["space"]:
+                    space_time += 30
+                    space = True
+                elif food_type[i-1] == ["time"]:
+                    time_time += 30
+                    time = True
+                #screen = pygame.display.set_mode((size_x,size_y))
+                #screen.fill(green)
+            if size_x < food_x[i-1]-5 or size_y < food_y[i-1]-5 :
+                food_x[i-1] = round(random.randrange(10,size_x - 10)/10)*10
+                food_y[i-1] = round(random.randrange(10,size_y - 10)/10)*10
+        #screen = pygame.display.set_mode((size_x,size_y))
+
         pygame.display.update()
 
-        if snake_x == food_x-5 and snake_y == food_y-5 :
-            food_x = round(random.randrange(10,size_x - 10)/10)*10
-            food_y = round(random.randrange(10,size_y - 10)/10)*10
-            snake_length += 1
-            size_y = size_y-10
-            size_x = size_x-10
-            speed += 0.1
-            screen = pygame.display.set_mode((size_x,size_y))
-
+        #print(food_x)
+        #print(food_y)
+        #print(size_x)
+        #print(size_y)
+        #print()
         clock.tick(temp_speed)
 
     pygame.quit()
     quit()
-
+#starting loop
 game_loop()
